f80577a5c090f8364cff04d730fc6cc0bdd7a17a
akgzkubilay <kubilayakgoz@ogr.bandirma.edu.tr>
2024-03-24 01:25:00 +0300

    iş kuralları eklendi

--- src/main/java/kodlama/io/Kodlama/io/Devs/business/abstracts/concretes/LanguageManager.java
+++ src/main/java/kodlama/io/Kodlama/io/Devs/business/abstracts/concretes/LanguageManager.java
@@ -6,35 +6,44 @@ import kodlama.io.Kodlama.io.Devs.business.abstracts.LanguageService;
 import kodlama.io.Kodlama.io.Devs.entities.concretes.Language;
 
 public class LanguageManager implements LanguageService{
+    private LanguageService languageService;
+    public LanguageManager(LanguageService languageService){
+        this.languageService = languageService;
+    }
 
     @Override
     public List<Language> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        return this.languageService.getAll();
     }
 
     @Override
     public Language getById(int id) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
-    }
+        if(id == 0){
+            throw new Exception("Id boş olamaz");
 
-    @Override
-    public void add(Language language) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
-    }
-
-    @Override
-    public void uptade(Language language, int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'uptade'");
+        }
+        return this.languageService.getById(id);
     }
 
     @Override
     public void delete(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    
+        this.languageService.delete(id);
+        
+    }
+
+    @Override
+    public void add(Language language) throws Exception {
+        if( language.getName().isEmpty()){
+            throw new Exception("Dil adı boş olamaz");
+        }
+        this.languageService.add(language);
+    }
+
+    @Override
+    public void uptade(Language language, int id) {
+        this.languageService.uptade(language, id);
+        
     }
 
     
